aef119a41714c347c18a760d4be25c612b4c4d53
Svetlana <sveta_36@list.ru>
2023-02-23 14:21:51 +0400

    add solution

--- Fence.java
+++ Fence.java
@@ -3,6 +3,23 @@
 //Напишите программу, которая поможет по длине забора (введенной пользователем) выяснить,
 // поместится ли на забор такой длины надпись мальчика.
 
+
 public class Fence {
 
+    final int sumLetters = 15;
+    final int sumSpace = 3;
+    final int lengthThreeLetters = 62;
+    final int lengthSpace = 12;
+
+    //Вычисление минимальной длины забора
+    int minLength = lengthThreeLetters * sumLetters / sumSpace + lengthSpace * sumSpace;
+
+    public void compareSumLength (int length) {
+
+        if (length >= minLength) {
+            System.out.println("Надпись поместится на забор " + Math.floorDiv(length, minLength) + " раз");
+        } else {
+            System.out.println("Надпись не поместится");
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите длину забора");
+        int length = sc.nextInt();
+        Fence fence1 = new Fence();
+        fence1.compareSumLength(length);
+        sc.close();
 
     }
 }
